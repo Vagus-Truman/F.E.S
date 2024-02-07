78dcf00a30a2ee3d78434ccd6d88a16f7de8a40f
Vagus-Truman <136032433+Vagus-Truman@users.noreply.github.com>
2024-02-07 13:44:51 -0500

    Effects.java

--- Effects.java
+++ Effects.java
@@ -1,9 +1,19 @@
 // it should be stated now that the board game will be based on a simple card combat system using dice to determine results
+// the agreed upon effects are as listed: fire, electric, psychic, and wind
 
 public class Effects {
 	
 	// CLASS FIELDS
+	private double multiplier = 1.0;
+	private double nerf = 0.5;
+	private boolean isStunned = false;
+	private boolean isBurning = false;
+	private boolean isShocked = false;
 	
+	// GETTERS
+	
+	
+	// SETTERS
 	
 	public static void main(String[] args) {
 		
