ff5b14b622fdf3e6bc769d848d0da5cdf7d8c6f6
Vagus-Truman <136032433+Vagus-Truman@users.noreply.github.com>
2024-03-01 20:37:45 -0500

    Update EffectBETA.java

--- EffectBETA.java
+++ EffectBETA.java
@@ -4,23 +4,12 @@
 import java.util.List;
 
 public class Effect {
-	static boolean isStunned = false;
-	static boolean isBurning = false;
-	static boolean isShocked = false;
-	private double fireDamage;
-	private double electricDamage;
+	public static boolean isStunned = false;
+	public static boolean isBurning = false;
+	public static boolean isShocked = false;
 	
 	public Effect() {
 		// deafauly constructosrs
-	}
-
-	public Effect(double fireDamage, double electricDamage) {
-		this.setFireDamage(fireDamage);
-		this.setElectricDamage(electricDamage);
-		// no effects applied in intial turn
-		this.isBurning = false;
-		this.isShocked = false;
-		this.isStunned = false;
 	}
 
 	// Getters and setters for damage fields omitted for brevity
@@ -34,28 +23,12 @@ public class Effect {
 		isBurning = burning;
 	}
 
-	public double getFireDamage() {
-		return fireDamage;
-	}
-
-	public void setFireDamage(double fireDamage) {
-		this.fireDamage = fireDamage;
-	}
-
 	public boolean isShocked() {
 		return isShocked;
 	}
 
 	public void setShocked(boolean shocked) {
 		isShocked = shocked;
-	}
-
-	public double getElectricDamage() {
-		return electricDamage;
-	}
-
-	public void setElectricDamage(double electricDamage) {
-		this.electricDamage = electricDamage;
 	}
 
 	public boolean isStunned() {
@@ -68,8 +41,6 @@ public class Effect {
 
 	// Method to reset damage values and effects
 	public void reset() {
-		this.setFireDamage(0);
-		this.setElectricDamage(0);
 		this.isBurning = false;
 		this.isShocked = false;
 		this.isStunned = false;
